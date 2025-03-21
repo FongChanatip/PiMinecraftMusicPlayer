04df9f9e4b88a33993a9fe5e9c83c29c20e451e9
FongChanatip <fongchanatip@gmail.com>
2025-03-20 17:40:41 -0700

    Completed main

--- PiMinecraftMusicPlayer/src/main.rs
+++ PiMinecraftMusicPlayer/src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use scheduler::scheduler::schedule_cron;
+use player::play_song;
+
+mod scheduler;
+mod external_factors;
+mod player;
+
+#[tokio::main]
+async fn main() {
+    let args: Vec<String> = env::args().collect();
+    let mode = args[1].parse::<i32>().unwrap();;
+    
+    player::song_picker::load_song_data();
+
+    if mode == 0{
+        schedule_cron();
+    } else {
+        play_song().await;
+    }
 }
